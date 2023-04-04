980f2af633b70ea352bae7875465360e792cc716
glt3953 <395313544@qq.com>
2023-04-04 15:03:14 +0800

    增加Fasttext和T5大模型的使用

---
+++ AI 大模型之美/04_classification_comparison_402.py
@@ -0,0 +1,42 @@
+
+from transformers import T5Tokenizer, T5Model
+from openai.embeddings_utils import cosine_similarity
+
+import torch
+
+# load the T5 tokenizer and model
+tokenizer = T5Tokenizer.from_pretrained('t5-base', model_max_length=512)
+model = T5Model.from_pretrained('t5-base')
+
+# set the model to evaluation mode
+model.eval()
+
+# encode the input sentence
+def get_t5_vector(line):
+    input_ids = tokenizer.encode(line, return_tensors='pt', max_length=512, truncation=True)
+    # generate the vector representation
+    with torch.no_grad():
+        outputs = model.encoder(input_ids=input_ids)
+        vector = outputs.last_hidden_state.mean(dim=1)
+    return vector[0]
+
+positive_text = """Wanted to save some to bring to my Chicago family but my North Carolina family ate all 4 boxes before I could pack. These are excellent...could serve to anyone"""
+negative_text = """First, these should be called Mac - Coconut bars, as Coconut is the #2 ingredient and Mango is #3. Second, lots of people don't like coconut. I happen to be allergic to it. Word to Amazon that if you want happy customers to make things like this more prominent. Thanks."""
+
+positive_review_in_t5 = get_t5_vector("An Amazon review with a positive sentiment.")
+negative_review_in_t5 = get_t5_vector('An Amazon review with a negative sentiment.')
+
+def test_t5():
+  positive_example_in_t5 = get_t5_vector(positive_text)
+  negative_example_in_t5 = get_t5_vector(negative_text)
+
+  def get_t5_score(sample_embedding):
+    return cosine_similarity(sample_embedding, positive_review_in_t5) - cosine_similarity(sample_embedding, negative_review_in_t5)
+
+  positive_score = get_t5_score(positive_example_in_t5)
+  negative_score = get_t5_score(negative_example_in_t5)
+
+  print("T5好评例子的评分 : %f" % (positive_score))
+  print("T5差评例子的评分 : %f" % (negative_score))
+
+test_t5()
---
+++ AI 大模型之美/04_classification_comparison_403.py
@@ -0,0 +1,48 @@
+
+import pandas as pd
+import torch
+
+from sklearn.metrics import classification_report
+from transformers import T5Tokenizer, T5Model
+from openai.embeddings_utils import cosine_similarity
+
+# load the T5 tokenizer and model
+tokenizer = T5Tokenizer.from_pretrained('t5-base', model_max_length=512)
+model = T5Model.from_pretrained('t5-base')
+
+datafile_path = "data/fine_food_reviews_with_embeddings_1k.csv"
+
+# encode the input sentence
+def get_t5_vector(line):
+    input_ids = tokenizer.encode(line, return_tensors='pt', max_length=512, truncation=True)
+    # generate the vector representation
+    with torch.no_grad():
+        outputs = model.encoder(input_ids=input_ids)
+        vector = outputs.last_hidden_state.mean(dim=1)
+    return vector[0]
+
+positive_review_in_t5 = get_t5_vector("An Amazon review with a positive sentiment.")
+negative_review_in_t5 = get_t5_vector('An Amazon review with a negative sentiment.')
+
+df = pd.read_csv(datafile_path)
+
+df["t5_embedding"] = df.Text.apply(get_t5_vector)
+# convert 5-star rating to binary sentiment
+df = df[df.Score != 3]
+df["sentiment"] = df.Score.replace({1: "negative", 2: "negative", 4: "positive", 5: "positive"})
+
+from sklearn.metrics import PrecisionRecallDisplay
+def evaluate_embeddings_approach():
+    def label_score(review_embedding):
+        return cosine_similarity(review_embedding, positive_review_in_t5) - cosine_similarity(review_embedding, negative_review_in_t5)
+
+    probas = df["t5_embedding"].apply(lambda x: label_score(x))
+    preds = probas.apply(lambda x: 'positive' if x>0 else 'negative')
+
+    report = classification_report(df.sentiment, preds)
+    print(report)
+
+    display = PrecisionRecallDisplay.from_predictions(df.sentiment, probas, pos_label='positive')
+    _ = display.ax_.set_title("2-class Precision-Recall curve")
+
+evaluate_embeddings_approach()
