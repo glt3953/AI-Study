464a83a2867cc7778618ce95b4f07421a7819728
glt3953 <395313544@qq.com>
2023-04-10 17:18:56 +0800

    增加搜索

--- AI 大模型之美/09_semantic_search_903.py
+++ AI 大模型之美/09_semantic_search_903.py
@@ -17,11 +17,11 @@ def generate_data_by_prompt(prompt):
     )
     return response.choices[0].text
 
-prompt = """请你生成100条淘宝网里的商品的标题，每条在30个字左右，品类是女性的服饰箱包等等，标题里往往也会有一些促销类的信息，每行一条。"""
+prompt = """请你生成100条淘宝网里的商品的标题，每条在30个字左右，品类是3C数码产品，标题里往往也会有一些促销类的信息，每行一条。"""
 data = generate_data_by_prompt(prompt)
 product_names = data.strip().split('\n')
 df = pd.DataFrame({'product_name': product_names})
-df.product_name = df.product_name.apply(lambda x: x.split('.')[1].strip())
+df.product_name = df.product_name.apply(lambda x: x.split('.')[0].strip())
 df.head()
 
 batch_size = 100
@@ -31,7 +31,7 @@ def get_embeddings_with_backoff(prompts, engine):
     embeddings = []
     for i in range(0, len(prompts), batch_size):
         batch = prompts[i:i+batch_size]
-        embeddings += get_embeddings(list_of_text=batch, engine=engine)
+        embeddings += get_embeddings(prompts=batch, engine=engine)
     return embeddings
 
 prompts = df.product_name.tolist()
@@ -43,14 +43,10 @@ for batch in prompt_batches:
     embeddings += batch_embeddings
 
 df["embedding"] = embeddings
-df.to_parquet("data/taobao_product_title.parquet", index=False)
+df.to_parquet("data/taobao_3C_product_title.parquet", index=False)
 
-# search through the reviews for a specific product
-def search_product(df, query, n=3, pprint=True):
-    product_embedding = get_embedding(
-        query,
-        engine=embedding_model
-    )
+def recommend_product(df, product_name, n=3, pprint=True):
+    product_embedding = df[df['product_name'] == product_name].iloc[0].embedding
     df["similarity"] = df.embedding.apply(lambda x: cosine_similarity(x, product_embedding))
 
     results = (
@@ -63,5 +59,5 @@ def search_product(df, query, n=3, pprint=True):
             print(r)
     return results
 
-results = search_product(df, "自然淡雅背包", n=3)
+results = recommend_product(df, "【限量】OnePlus 7T Pro 8GB+256GB 全网通", n=3)
 display(results)
