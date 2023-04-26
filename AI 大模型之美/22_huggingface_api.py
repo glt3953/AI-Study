4e01c93e74b5154dfd7f55bbf0df50784c060b10
glt3953 <395313544@qq.com>
2023-04-26 14:32:35 +0800

    增加“搭建翻译系统”

--- AI 大模型之美/22_huggingface_api.py
+++ AI 大模型之美/22_huggingface_api.py
@@ -2,7 +2,9 @@ import os, requests, json
 
 API_TOKEN = os.environ.get("HUGGINGFACE_API_KEY")
 
+#model = "google/flan-t5-xxl"
 model = "Helsinki-NLP/opus-mt-en-zh"
+#model = "hfl/chinese-pert-base"
 
 API_URL = f"https://api-inference.huggingface.co/models/{model}"
 headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "application/json"}
@@ -12,11 +14,9 @@ def query(payload, api_url=API_URL, headers=headers):
     response = requests.request("POST", api_url, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
 
-# 翻译文件
-with open('data/wwdc2019-423-demo.txt') as f:
-    for line in f:
-        content = line
-        data = query({"inputs" : content, "wait_for_model" : True})
-        print(data)
-        with open('data/wwdc2019-423-demo-zh.txt', 'a', encoding='utf-8') as f:
-            f.write(''.join(data[0].values()) + '\n') # 将dict的值连接为字符串
+#question = "Please answer the following question. What is the capital of France?"
+question = "My name is Wolfgang and I live in Berlin"
+#question = "今天天气真不错！"
+data = query({"inputs" : question, "wait_for_model" : True})
+
+print(data)
