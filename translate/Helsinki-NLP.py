29790f44196ecfbd0174c81c06e4ef4afe79de13
glt3953 <395313544@qq.com>
2023-04-26 17:14:51 +0800

    增加Helsinki-NLP

--- translate/Helsinki-NLP.py
+++ translate/Helsinki-NLP.py
@@ -1,22 +1,24 @@
-import os, requests, json
+import torch
+torch.cuda.is_available()
 
-API_TOKEN = os.environ.get("HUGGINGFACE_API_KEY")
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-model = "Helsinki-NLP/opus-mt-en-zh"
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-zh")
 
-API_URL = f"https://api-inference.huggingface.co/models/{model}"
-headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "application/json"}
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-zh")
 
-def query(payload, api_url=API_URL, headers=headers):
-    data = json.dumps(payload)
-    response = requests.request("POST", api_url, headers=headers, data=data)
-    return json.loads(response.content.decode("utf-8"))
+text = "I like to learn data science and AI."
+# text = "我喜欢学习数据科学和人工智能。"
 
 # 翻译文件
-with open('data/wwdc2019-423-demo.txt') as f:
-    for line in f:
-        content = line
-        data = query({"inputs" : content, "wait_for_model" : True})
-        print(data)
-        with open('data/wwdc2019-423-demo-zh.txt', 'a', encoding='utf-8') as f:
-            f.write(''.join(data[0].values()) + '\n') # 将dict的值连接为字符串
+# with open('drive/MyDrive/colab_data/article/wwdc2019-423.txt') as f:
+#     for line in f:
+#         content = line
+#         translated_text = translation(content)
+#         print(translated_text)
+#         with open('drive/MyDrive/colab_data/article/wwdc2019-423-zh.txt', 'a', encoding='utf-8') as f:
+#           f.write(''.join(translated_text[0].values()) + '\n') # 将dict的值连接为字符串
+
+translated_text = model.translate(text)
+
+print(translated_text)
---
+++ translate/difflib_file.py
@@ -0,0 +1,21 @@
+import difflib
+
+#with open('./data/wwdc2019-423-zh.txt') as f:
+#    medium_text = f.read()
+#
+#with open('./data/wwdc2019-423-trans-opus-mt-en-zh.txt') as f:
+#    openai_text = f.read()
+#
+##diff = difflib.ndiff(medium_text.splitlines(keepends=True), openai_text.splitlines(keepends=True))
+#diff = difflib.context_diff(medium_text.splitlines(keepends=True), openai_text.splitlines(keepends=True))
+#print(''.join(diff), end='')
+
+#diff = difflib.HtmlDiff()
+#result = diff.make_file(medium_text.splitlines(), openai_text.splitlines())
+#with open('./data/podcast_clip_diff.html', "w") as f:
+#    f.write(result)
+
+with open('./data/wwdc2019-423-zh.txt') as f1, open('./data/wwdc2019-423-Hgn_trans_en2zh.txt') as f2:
+    diff = difflib.unified_diff(f1.readlines(), f2.readlines())
+for line in diff:
+    print(line)  # 打印差异内容
