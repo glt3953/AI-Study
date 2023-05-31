43bd0bf0ca7563b0319b20011269e2b4b54c486d
glt3953 <395313544@qq.com>
2023-05-31 12:33:50 +0800

    增加“本地搭建英译中”

--- translate/Helsinki-NLP_API.py
+++ translate/Helsinki-NLP_API.py
@@ -2,7 +2,7 @@ import os, requests, json
 
 API_TOKEN = os.environ.get("HUGGINGFACE_API_KEY")
 
-model = "Helsinki-NLP/opus-mt-en-zh"
+model = "Helsinki-NLP/opus-mt-zh-en" #Helsinki-NLP/opus-mt-en-zh、Helsinki-NLP/opus-mt-zh-en
 
 API_URL = f"https://api-inference.huggingface.co/models/{model}"
 headers = {"Authorization": f"Bearer {API_TOKEN}", "Content-Type": "application/json"}
@@ -12,11 +12,17 @@ def query(payload, api_url=API_URL, headers=headers):
     response = requests.request("POST", api_url, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
 
+# 翻译文本
+content = "苹果是一家美国跨国科技公司。"
+#content = "Apple is an American multinational technology company."
+result = query({"inputs" : content, "wait_for_model" : True})
+print(result)
+        
 # 翻译文件
-with open('data/wwdc2019-423-demo.txt') as f:
-    for line in f:
-        content = line
-        data = query({"inputs" : content, "wait_for_model" : True})
-        print(data)
-        with open('data/wwdc2019-423-demo-zh.txt', 'a', encoding='utf-8') as f:
-            f.write(''.join(data[0].values()) + '\n') # 将dict的值连接为字符串
+#with open('data/wwdc2019-423-demo.txt') as f:
+#    for line in f:
+#        content = line
+#        data = query({"inputs" : content, "wait_for_model" : True})
+#        print(data)
+#        with open('data/wwdc2019-423-demo-zh.txt', 'a', encoding='utf-8') as f:
+#            f.write(''.join(data[0].values()) + '\n') # 将dict的值连接为字符串
--- translate/translatetk.py
+++ translate/translatetk.py
@@ -1,27 +1,34 @@
-#from translatetk import Translator
-#
-## 初始化Translator对象,指定源语言和目标语言
-#translator = Translator("en", "zh")
-#
-## 进行翻译,返回翻译结果
-#translation = translator.translate("Hello, world!")
-#print(translation)
-## 你好,世界!
-#
-## 也可以翻译整段文本
-#text = "Apple is an American multinational technology company that specializes in consumer electronics, software and online services."
-#translation = translator.translate(text)
-#print(translation)
-## 苹果是一家美国跨国科技公司,专门从事消费电子产品、软件和在线服务。
-
+#pip install translate
 from translate import Translator
 
-translator= Translator(to_lang="zh")
-translation = translator.translate("Hello, world!")
-print(translation)
-# 你好,世界!
+# 初始化Translator对象,指定源语言和目标语言
+translator = Translator(from_lang="zh", to_lang="en")
 
-text = "Apple is an American multinational technology company."
+# 进行翻译,返回翻译结果
+text = "虎虎生威!"
 translation = translator.translate(text)
 print(translation)
+# 你好,世界!
+# Hello, world!
+
+# 也可以翻译整段文本
+text = "苹果是一家美国跨国科技公司,专门从事消费电子产品、软件和在线服务。"
+translation = translator.translate(text)
+print(translation)
+# 苹果是一家美国跨国科技公司,专门从事消费电子产品、软件和在线服务。
+# Apple is an American multinational technology company that specializes in consumer electronics, software and online services.
+
+#from translate import Translator
+#
+##translator = Translator(to_lang="en")
+#translator = Translator("zh", "en")
+#translation = translator.translate("你好,世界!")
+#print(translation)
+# 你好,世界!
+# Hello, world!
+
+#text = "苹果是一家美国跨国科技公司。"
+#translation = translator.translate(text)
+#print(translation)
 # 苹果是一家美国跨国科技公司。
+# Apple is an American multinational technology company.
