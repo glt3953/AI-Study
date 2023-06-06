481d3b97e19aed1e7fc0dba89cbd8e128ea9b27b
glt3953 <395313544@qq.com>
2023-06-06 10:51:01 +0800

    争取获取北京时间

--- cv_unet_person-image-cartoon/app-person-image-cartoon.py
+++ cv_unet_person-image-cartoon/app-person-image-cartoon.py
@@ -37,7 +37,7 @@ def decode_image(raw_image):
 
 #获取当前北京时间
 utc_dt = datetime.datetime.utcnow()
-beijing_dt = utc_dt.astimezone(datetime.timezone(datetime.timedelta(hours=8)))
+beijing_dt = utc_dt.astimezone(datetime.timezone(datetime.timedelta(hours=16)))
 formatted = beijing_dt.strftime("%Y-%m-%d_%H")
 print(f"北京时间: {beijing_dt.year}年{beijing_dt.month}月{beijing_dt.day}日 "
       f"{beijing_dt.hour}时{beijing_dt.minute}分{beijing_dt.second}秒")
